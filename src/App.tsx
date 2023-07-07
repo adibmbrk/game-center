acbb2d3ec7f9706f567f94a0af1ea8e9c8863dbb
Adib <adibmbrk@gmail.com>
2023-07-07 10:13:42 +0530

    highlighting selected genre

--- src/App.tsx
+++ src/App.tsx
@@ -7,30 +7,34 @@ import { Genre } from "./hooks/useGenres";
 import { useState } from "react";
 
 function App() {
-const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
+  const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
 
   return (
     <>
-      <Grid templateAreas={{
-        base: '"nav" "main"',
-        lg: '"nav nav" "aside main"'
-      }}
+      <Grid
+        templateAreas={{
+          base: '"nav" "main"',
+          lg: '"nav nav" "aside main"',
+        }}
         templateColumns={{
-          base: '1fr',
-          lg: '200px 1fr'
+          base: "1fr",
+          lg: "200px 1fr",
         }}
       >
         <GridItem area="nav">
           <Navbar />
         </GridItem>
         <Show above="lg">
-          <GridItem area="aside" paddingX={5} >
-            <GenreList onSelectGenre={(genre) => setSelectedGenre(genre)} />
+          <GridItem area="aside" paddingX={5}>
+            <GenreList
+              selectedGenre={selectedGenre}
+              onSelectGenre={(genre) => setSelectedGenre(genre)}
+            />
           </GridItem>
         </Show>
-          
+
         <GridItem area="main">
-          <GameGrid selectedGenre={selectedGenre}/>
+          <GameGrid selectedGenre={selectedGenre} />
         </GridItem>
       </Grid>
     </>
